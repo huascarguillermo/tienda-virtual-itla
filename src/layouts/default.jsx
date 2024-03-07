8f66382dc9d5beb5891a4477f09239cd0ed0e32b
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2024-03-06 23:54:14 -0400

    Add Navbar and Avatar to DefaultLayout component

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,13 +1,68 @@
-import { Outlet } from 'react-router-dom';
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	Avatar,
+	DropdownMenu,
+	DropdownItem,
+	Dropdown,
+	DropdownTrigger
+} from '@nextui-org/react';
+import { Outlet, Link } from 'react-router-dom';
+
+import styles from './default.module.css';
 
 const DefaultLayout = () => {
 	return (
 		<>
-			<header>
-				<h1>Hey</h1>
-			</header>
+			<Navbar shouldHideOnScroll maxWidth='xl'>
+				<NavbarBrand>
+					<Link to='/' className={`${styles.link} rounded-full p-2`}>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='20'
+							height='20'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+						>
+							<path d='M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z'></path>
+							<line x1='3' y1='6' x2='21' y2='6'></line>
+							<path d='M16 10a4 4 0 0 1-8 0'></path>
+						</svg>
+					</Link>
+				</NavbarBrand>
+				<NavbarContent as='div' justify='end'>
+					<Dropdown placement='bottom-end'>
+						<DropdownTrigger>
+							<Avatar
+								isBordered
+								as='button'
+								className='transition-transform'
+								color='primary'
+								name='JR'
+								size='sm'
+							/>
+						</DropdownTrigger>
+						<DropdownMenu aria-label='Profile Actions' variant='flat'>
+							<DropdownItem key='profile' className='h-14 gap-2'>
+								<p className='font-semibold'>Signed in as</p>
+								<p className='font-semibold'>zoey@example.com</p>
+							</DropdownItem>
+							<DropdownItem key='logout' color='danger'>
+								Log Out
+							</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</NavbarContent>
+			</Navbar>
 
-			<Outlet />
+			<main>
+				<Outlet />
+			</main>
 		</>
 	);
 };

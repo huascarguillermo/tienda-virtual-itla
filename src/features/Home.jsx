6e679db05b2d7e18bf1cb45d329f4ad1c8ad6a11
JeanCRuiz <139879016+JeanCRuiz@users.noreply.github.com>
2024-03-07 15:57:46 -0400

    Refactor Home component to fetch and display products

--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from 'react';
 import { getProducts } from '../services/fakeStoreApi';
 
 function Home() {
-	getProducts();
+	const [products, setProducts] = useState([]);
+	const [isLaoding, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+
+		getProducts()
+			.then(res => {
+				const data = res.data;
+				setProducts(data);
+
+				console.log(data);
+			})
+			.catch(err => {
+				if (err.response) {
+					console.log(err.response.data);
+					console.log(err.response.status);
+					console.log(err.response.headers);
+				} else if (err.request) {
+					console.log(err.request);
+				} else {
+					console.log('Something went wrong!', err.message);
+				}
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	}, []);
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<section>
+			<h2>Products:</h2>
+
+			{isLaoding ? (
+				<p>Loading...</p>
+			) : (
+				<ul>
+					{products.map((product, index) => (
+						<li key={product.id}>
+							<h2>{`${index + 1}-${product.title}`}</h2>
+						</li>
+					))}
+				</ul>
+			)}
+		</section>
 	);
 }
 

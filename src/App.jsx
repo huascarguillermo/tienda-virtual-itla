17a6f2088b0a6c2738ab35fcd1d3e53a3b5e0ff3
Huascar Guillermo Ulerio Carrasco <hguillermo59@gmail.com>
2024-03-06 21:18:08 -0400

    creating routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,27 @@ import {
 } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
 import DefaultLayout from './layouts/default';
+import { Cart, Checkout, Home, Login, Register } from './features/index.js';
 
 const router = createBrowserRouter([
 	{
 		element: <DefaultLayout />,
 		children: [
-			{ path: '/', element: <h1>Home</h1> },
-			{ path: '/login', element: <h1>Login</h1> },
-			{ path: '/register', element: <h1>Register</h1> },
-			{ path: '/cart', element: <h1>Cart</h1> },
-			{ path: '/checkout', element: <h1>Checkout</h1> }
+			{ path: '/', element: <Home /> },
+			{ path: '/login', element: <Login /> },
+			{ path: '/register', element: <Register /> },
+			{ path: '/cart', element: <Cart /> },
+			{ path: '/checkout', element: <Checkout /> }
 		]
 	}
 ]);
 
 function App() {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
 	return (
-		<NextUIProvider navigate={navigate}>
+		// <NextUIProvider navigate={navigate}>
+		<NextUIProvider>
 			<RouterProvider router={router} />
 		</NextUIProvider>
 	);

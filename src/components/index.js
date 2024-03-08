fe78bdd938c73129152fbb3e45d792ebe4f3633d
Huascar Guillermo Ulerio Carrasco <hguillermo59@gmail.com>
2024-03-07 21:25:41 -0400

    Login page updated

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as MailIcon } from './MailIcon.jsx'
+export { default as EyeOpen } from './EyeOpenIcon.jsx'
+export { default as EyeClosed } from './EyeClosedIcon.jsx'
+export { default as Lock } from './Lock.jsx'
